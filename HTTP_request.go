1813ea1508727b71bf4c9653c07dd8f50ac44700
varsha0713 <99706376+varsha0713@users.noreply.github.com>
2023-09-01 07:20:47 +0530

    commit

--- HTTP_request.go
+++ HTTP_request.go
@@ -26,33 +26,25 @@ func main() {
 	nostrum rerum est autem sunt rem eveniet architecto
 	*/
 
-	// URL to send the GET request
+	
 	url := "https://jsonplaceholder.typicode.com/posts/1"
-
-	// send the GET request
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("HTTP GET request failed: ", err)
 		return
 	}
-	// Ensure that the response body is closed after we're done with it
 	defer resp.Body.Close()
-
-	// Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading the response body: ", err)
 		return
 	}
-	// Parse the JSON response into a Post struct
 	var post Post
 	err = json.Unmarshal(body, &post)
 	if err != nil {
 		fmt.Println("Error parsing the JSON response: ", err)
 		return
 	}
-
-	// Print the parsed post
 	fmt.Printf("User Id: %d\n", post.UserId)
 	fmt.Printf("Id: %d\n", post.Id)
 	fmt.Printf("Title: %s\n", post.Title)
