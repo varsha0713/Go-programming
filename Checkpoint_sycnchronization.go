cc31cfd82f0b29b41e95f8fc11b68676bee69e95
varsha0713 <99706376+varsha0713@users.noreply.github.com>
2023-09-01 09:01:26 +0530

    commit

--- Checkpoint_sycnchronization.go
+++ Checkpoint_sycnchronization.go
@@ -9,16 +9,16 @@ import (
 func worker(id int, checkpoint chan bool, resume chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	fmt.Printf("Worker %d : Starintng work\n", id)
-	time.Sleep(time.Duration(id) * time.Second) // Simulate work
+	fmt.Printf("Worker %d : Starting work\n", id)
+	time.Sleep(time.Duration(id) * time.Second) 
 
 	fmt.Printf("Worker %d : Checkpoint Reached\n", id)
-	checkpoint <- true // signal that checkpoint is reached
+	checkpoint <- true 
 
-	<-resume // wait for resume signal
+	<-resume 
 
 	fmt.Printf("Worker %d : Resuming work\n", id)
-	//Continue with the remaining work
+	
 }
 
 func main() {
@@ -27,13 +27,10 @@ func main() {
 	resume := make(chan bool)
 	var wg sync.WaitGroup
 
-	//Launch the worker goroutines
 	for i := 1; i <= numWorkers; i++ {
 		wg.Add(1)
 		go worker(i, checkpoint, resume, &wg)
 	}
-
-	//Wait for all the workers to reach the checkpoint
 	for i := 1; i <= numWorkers; i++ {
 		<-checkpoint
 	}
@@ -41,7 +38,6 @@ func main() {
 	fmt.Println("All workers reached the checkpoint")
 	fmt.Println("Resuming work now")
 
-	//Signal all the workers to resume work
 	for i := 1; i <= numWorkers; i++ {
 		resume <- true
 	}
